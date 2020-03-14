25b3233315ea3e32fdcd893b52ef83f4689f57e3
Facuuu <Facuuu@FACU-ULTRABOOK>
2020-03-14 13:52:37 -0300

    Versión inicial

---
+++ turnos-api/src/main/java/com/turnos/app/TurnosApiApplication.java
@@ -0,0 +1,13 @@
+package com.turnos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosApiApplication.class, args);
+	}
+
+}
---
+++ turnos-api/src/test/java/com/turnos/app/TurnosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
