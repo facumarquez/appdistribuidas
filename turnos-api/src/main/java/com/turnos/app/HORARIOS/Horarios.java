745f38e94a8f1c6e3727092e5e00e43625ef7c42
Sergio <gonzalez.sergiod@gmail.com>
2020-03-28 16:58:08 -0300

    Se agrega entidad horarios

---
+++ turnos-api/src/main/java/com/turnos/app/HORARIOS/Horarios.java
@@ -0,0 +1,44 @@
+package com.turnos.app.HORARIOS;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="Horarios")
+public class Horarios {
+
+	@Id
+	@Column(name="idHorario")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String HorarioDesde;
+	
+	@Column
+	private String HorarioHasta;
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getHorarioDesde() {
+		return HorarioDesde;
+	}
+
+	public void setHorarioDesde(String horarioDesde) {
+		HorarioDesde = horarioDesde;
+	}
+
+	public String getHorarioHasta() {
+		return HorarioHasta;
+	}
+
+	public void setHorarioHasta(String horarioHasta) {
+		HorarioHasta = horarioHasta;
+	}
+}
