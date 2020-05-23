571e2ed99c7a80dec4710cec6e1de004ea05fc6c
Sergio <gonzalez.sergiod@gmail.com>
2020-05-23 19:50:23 -0300

    Se crea test para probar la generacion de turnos por franja horaria

--- turnos-api/src/test/java/com/turnos/app/TurnosApiApplicationTests.java
+++ turnos-api/src/test/java/com/turnos/app/TurnosApiApplicationTests.java
@@ -1,7 +1,15 @@
 package com.turnos.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.turnos.app.AGENDAMEDICOTURNO.AgendaMedicoTurno;
+import com.turnos.app.HELPERS.TurnoHelper;
 
 @SpringBootTest
 class TurnosApiApplicationTests {
@@ -9,5 +17,14 @@ class TurnosApiApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+	public void testGeneradorDeTurnos() {
+		
+		List<AgendaMedicoTurno> turnos = new ArrayList<AgendaMedicoTurno>();
+		
+		turnos = TurnoHelper.generarTurnos("09:00", "18:00", null);
+		//Se deberian generar 36 turnos (4 turnos x hora) x 9 hs(9 a 18)...
+		assertEquals(turnos.size(),36);
+	}
 }
